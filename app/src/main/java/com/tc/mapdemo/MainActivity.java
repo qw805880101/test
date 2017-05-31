287b69fbfce6cdeb9733df407163d370b2d28951
admin <qw805880101@qq.com>
2017-05-31 22:31:22 +0800

    tc

--- app/src/main/java/com/tc/mapdemo/MainActivity.java
+++ app/src/main/java/com/tc/mapdemo/MainActivity.java
@@ -117,7 +117,7 @@ public class MainActivity extends Activity implements OnClickListener, AMapLocat
 //        mLocationOption.setLocationMode(AMapLocationMode.Device_Sensors);
 
         //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
-        mLocationOption.setInterval(1000);
+        mLocationOption.setInterval(5000);
 
         //设置是否返回地址信息（默认返回地址信息）
         mLocationOption.setNeedAddress(true);
@@ -208,6 +208,15 @@ public class MainActivity extends Activity implements OnClickListener, AMapLocat
                     cityCode = amapLocation.getCityCode();
                     //停止定位
                     mLocationClient.stopLocation();
+
+                    MyLocationStyle myLocationStyle;
+                    myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+                    myLocationStyle.interval(5000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+                    aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+                    myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW) ;//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
+//aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+                    aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+
                 } else {
                     endLatlng = new LatLng(amapLocation.getLatitude(), amapLocation.getLongitude());
                     mPolyoptions.add(endLatlng);
